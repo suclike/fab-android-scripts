05b1a81d3e6774a829c5f296ad1c9c681fe797b2
aminezghidi <zghidi.amine@gmail.com>
2016-10-14 14:44:40 +0100

    Remove comments

--- fab-android-scripts.rb
+++ fab-android-scripts.rb
@@ -1,7 +1,3 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class FabAndroidScripts < Formula
   desc ""
   homepage "https://github.com/thefabulous/fab-android-scripts"
@@ -9,22 +5,17 @@ class FabAndroidScripts < Formula
   version "1.1.3"
   sha256 ""
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on :x11
 
   def install
     bin.install 'src/devtools.groovy' => 'devtools'
     bin.install 'src/adbwifi.groovy' => 'adbwifi'
     bin.install 'src/adbscreenrecord.groovy' => 'adbscreenrecord'
 
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 end
