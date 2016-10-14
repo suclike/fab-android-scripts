462999234a8eeedaa9b0f5ad638ddbbbcd2676ab
Amine Laadhari <amine.laadhari@gmail.com>
2016-10-14 17:02:14 +0200

    Remove sha256

--- fab-android-scripts.rb
+++ fab-android-scripts.rb
@@ -5,7 +5,7 @@ class FabAndroidScripts < Formula
   homepage "https://github.com/thefabulous/fab-android-scripts"
   url "https://github.com/thefabulous/fab-android-scripts/archive/master.tar.gz"
   version "1.1.3"
-  sha256 "7fdb794fbecb17b3d695d7a28b369a449b5afa5925d6fbba8242a17e6c7cb5e8"
+  sha256 ""
   head "https://github.com/thefabulous/fab-android-scripts.git"
 
   depends_on "groovy"
