6a4ef33f146d97abb6fcbd873ab398fbc9f45ed0
aminezghidi <zghidi.amine@gmail.com>
2016-10-14 15:31:19 +0100

    Add sha256 to the formulae

--- fab-android-scripts.rb
+++ fab-android-scripts.rb
@@ -3,7 +3,7 @@ class FabAndroidScripts < Formula
   homepage "https://github.com/thefabulous/fab-android-scripts"
   url "https://github.com/thefabulous/fab-android-scripts/archive/master.tar.gz"
   version "1.1.3"
-  sha256 ""
+  sha256 "324ce5d98c079a2feacbcde6f3e541599871910f9acebebbe9728353dd6fa12b"
 
   depends_on :x11
 
