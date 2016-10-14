89ee25943e03010bc2fec83691a312b864d7e5ad
aminezghidi <zghidi.amine@gmail.com>
2016-10-14 15:51:53 +0100

    Fix Formula

--- fab-android-scripts.rb
+++ fab-android-scripts.rb
@@ -1,21 +1,16 @@
 class FabAndroidScripts < Formula
-  desc ""
+  desc "Fab Internal Android Script Tools"
   homepage "https://github.com/thefabulous/fab-android-scripts"
   url "https://github.com/thefabulous/fab-android-scripts/archive/master.tar.gz"
   version "1.1.3"
   sha256 "324ce5d98c079a2feacbcde6f3e541599871910f9acebebbe9728353dd6fa12b"
+  head "https://github.com/thefabulous/fab-android-scripts.git"
 
-  depends_on :x11
+  depends_on "groovy"
 
   def install
     bin.install 'src/devtools.groovy' => 'devtools'
     bin.install 'src/adbwifi.groovy' => 'adbwifi'
     bin.install 'src/adbscreenrecord.groovy' => 'adbscreenrecord'
-
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
   end
 end
